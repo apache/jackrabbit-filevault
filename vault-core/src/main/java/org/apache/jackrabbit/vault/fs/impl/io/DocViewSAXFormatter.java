88a59b6c8d6d6eee019beabac5a97ad070ff198b
Tobias Bocanegra <tripod@apache.org>
2016-12-19 04:41:22 +0000

    JCRVLT-147 Vault serialization doesn't handle xmltext and xmlcharacter correctly
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/commons/filevault/trunk@1774986 13f79535-47bb-0310-9956-ffa450edef68

--- vault-core/src/main/java/org/apache/jackrabbit/vault/fs/impl/io/DocViewSAXFormatter.java
+++ vault-core/src/main/java/org/apache/jackrabbit/vault/fs/impl/io/DocViewSAXFormatter.java
@@ -23,19 +23,23 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.jcr.NamespaceException;
 import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 
 import org.apache.jackrabbit.spi.Name;
+import org.apache.jackrabbit.spi.commons.conversion.DefaultNamePathResolver;
 import org.apache.jackrabbit.spi.commons.conversion.NameException;
 import org.apache.jackrabbit.spi.commons.conversion.NameParser;
 import org.apache.jackrabbit.spi.commons.name.NameConstants;
 import org.apache.jackrabbit.spi.commons.name.NameFactoryImpl;
+import org.apache.jackrabbit.spi.commons.namespace.NamespaceResolver;
+import org.apache.jackrabbit.spi.commons.namespace.SessionNamespaceResolver;
 import org.apache.jackrabbit.util.ISO9075;
 import org.apache.jackrabbit.vault.fs.api.Aggregate;
 import org.apache.jackrabbit.vault.fs.api.VaultFsConfig;
-import org.apache.jackrabbit.vault.fs.impl.AggregateManagerImpl;
 import org.apache.jackrabbit.vault.util.DocViewProperty;
 import org.apache.jackrabbit.vault.util.ItemNameComparator;
 import org.apache.jackrabbit.vault.util.JcrConstants;
@@ -45,27 +49,135 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
 /**
- * {@code DocViewSAXFormatter}...
- *
+ * The docview sax formatter generates SAX events to a given ContentHandler based on the aggregate tree.
  */
-public class DocViewSAXFormatter extends AbstractSAXFormatter {
+public class DocViewSAXFormatter implements AggregateWalkListener {
 
+    /**
+     * The XML elements and attributes used in serialization
+     */
     public static final String CDATA_TYPE = "CDATA";
+
+    /**
+     * the session to be used for resolving namespace mappings
+     */
+    protected final Session session;
+
+    /**
+     * the session's namespace resolver
+     */
+    protected final NamespaceResolver nsResolver;
+
+    /**
+     * the content handler to feed the SAX events to
+     */
+    protected final ContentHandler contentHandler;
+
+    /**
+     * The jcr:primaryType property name (allowed for session-local prefix mappings)
+     */
+    protected final String jcrPrimaryType;
+
+    /**
+     * The nt:unstructured name (allowed for session-local prefix mappings)
+     */
+    protected final String ntUnstructured;
+
+    /**
+     * The jcr:mixinTypes property name (allowed for session-local prefix mappings)
+     */
+    protected final String jcrMixinTypes;
+
+    /**
+     * The jcr:uuid property name (allowed for session-local prefix mappings)
+     */
+    protected final String jcrUUID;
+
+    /**
+     * The jcr:root node name (allowed for session-local prefix mappings)
+     */
+    protected final String jcrRoot;
 
     // used to temporarily store properties of a node
     private final List<Property> props = new ArrayList<Property>();
 
-    private boolean useJcrRoot;
+    /**
+     * the export context
+     */
+    private final Aggregate aggregate;
 
-    private boolean useBinaryReferences;
+    /**
+     * flag indicating if binary references are to be used.
+     */
+    private final boolean useBinaryReferences;
 
-    private Set<String> ignored = new HashSet<String>();
+    /**
+     * internally ignored properties
+     */
+    private final Set<String> ignored = new HashSet<String>();
 
-    public DocViewSAXFormatter(Aggregate aggregate, ContentHandler contentHandler)
+    protected DocViewSAXFormatter(Aggregate aggregate, ContentHandler contentHandler)
             throws RepositoryException {
-        super(aggregate, contentHandler);
+
+        this.aggregate = aggregate;
+        this.session = aggregate.getNode().getSession();
+        nsResolver = new SessionNamespaceResolver(session);
+
+        this.contentHandler = contentHandler;
+
+        DefaultNamePathResolver npResolver = new DefaultNamePathResolver(nsResolver);
+
+        // resolve the names of some well known properties
+        // allowing for session-local prefix mappings
+        try {
+            jcrPrimaryType = npResolver.getJCRName(NameConstants.JCR_PRIMARYTYPE);
+            jcrMixinTypes = npResolver.getJCRName(NameConstants.JCR_MIXINTYPES);
+            jcrUUID = npResolver.getJCRName(NameConstants.JCR_UUID);
+            jcrRoot = npResolver.getJCRName(NameConstants.JCR_ROOT);
+            ntUnstructured = npResolver.getJCRName(NameConstants.NT_UNSTRUCTURED);
+        } catch (NamespaceException e) {
+            // should never get here...
+            String msg = "internal error: failed to resolve namespace mappings";
+            throw new RepositoryException(msg, e);
+        }
 
         useBinaryReferences = "true".equals(aggregate.getManager().getConfig().getProperty(VaultFsConfig.NAME_USE_BINARY_REFERENCES));
+    }
+
+    /**
+     * Starts namespace declarations
+     *
+     * @throws RepositoryException if a repository error occurs
+     * @throws SAXException if the underlying content handler throws a sax exception
+     */
+    protected void startNamespaceDeclarations()
+            throws RepositoryException, SAXException {
+        // start namespace declarations
+        for (String prefix: aggregate.getNamespacePrefixes()) {
+            if (Name.NS_XML_PREFIX.equals(prefix)) {
+                // skip 'xml' prefix as this would be an illegal namespace declaration
+                continue;
+            }
+            contentHandler.startPrefixMapping(prefix, aggregate.getNamespaceURI(prefix));
+        }
+    }
+
+    /**
+     * Ends namespace declarations
+     *
+     * @throws RepositoryException if a repository error occurs
+     * @throws SAXException if the underlying content handler throws a sax exception
+      */
+    protected void endNamespaceDeclarations()
+            throws RepositoryException, SAXException {
+        // end namespace declarations
+        for (String prefix: aggregate.getNamespacePrefixes()) {
+            if (Name.NS_XML_PREFIX.equals(prefix)) {
+                // skip 'xml' prefix as this would be an illegal namespace declaration
+                continue;
+            }
+            contentHandler.endPrefixMapping(prefix);
+        }
     }
 
     private Name getQName(String rawName) throws RepositoryException {
@@ -79,20 +191,10 @@ public class DocViewSAXFormatter extends AbstractSAXFormatter {
     }
 
     /**
-     * Controls if the jcr:root should be used as root element name.
-     * @param useJcrRoot {@code true} if jcrRoot is to be used as
-     * root element name.
-     */
-    public void setUseJcrRoot(boolean useJcrRoot) {
-        this.useJcrRoot = useJcrRoot;
-    }
-
-    /**
      * {@inheritDoc}
      */
+    @Override
     public void onWalkBegin(Node root) throws RepositoryException {
-        super.onWalkBegin(root);
-
         // init ignored protected properties
         ignored.clear();
         ignored.add(JcrConstants.JCR_CREATED);
@@ -100,85 +202,83 @@ public class DocViewSAXFormatter extends AbstractSAXFormatter {
         ignored.add(JcrConstants.JCR_BASEVERSION);
         ignored.add(JcrConstants.JCR_VERSIONHISTORY);
         ignored.add(JcrConstants.JCR_PREDECESSORS);
+
+        try {
+            contentHandler.startDocument();
+            startNamespaceDeclarations();
+        } catch (SAXException e) {
+            throw new RepositoryException(e);
+        }
     }
 
     /**
      * {@inheritDoc}
      */
+    @Override
+    public void onWalkEnd(Node root) throws RepositoryException {
+        try {
+            // clear namespace declarations and end document
+            endNamespaceDeclarations();
+            contentHandler.endDocument();
+        } catch (SAXException e) {
+            throw new RepositoryException(e);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
     public void onNodeBegin(Node node, boolean included, int level)
             throws RepositoryException{
         // register used node types
-        ((AggregateManagerImpl) aggregate.getManager()).addNodeTypes(node);
+        aggregate.getManager().addNodeTypes(node);
         props.clear();
     }
 
     /**
      * {@inheritDoc}
      */
+    @Override
     public void onChildren(Node node, int level) throws RepositoryException {
-        String name = node.getName();
         String label = Text.getName(node.getPath());
-        if (name.equals(jcrXMLText)) {
-            // the node represents xml character data
-            for (Property prop : props) {
-                String propName = prop.getName();
-                if (propName.equals(jcrXMLCharacters)) {
-                    // assume jcr:xmlcharacters is single-valued
-                    char[] chars = prop.getValue().getString().toCharArray();
-                    try {
-                        contentHandler.characters(chars, 0, chars.length);
-                    } catch (SAXException e) {
-                        throw new RepositoryException(e);
-                    }
-                }
-            }
+        String elemName;
+        if (level == 0 || node.getDepth() == 0) {
+            // root node needs a name
+            elemName = jcrRoot;
         } else {
-            // regular node
-            // element name
-            String elemName;
-            if ((level == 0 && useJcrRoot) || node.getDepth() == 0) {
-                // root node needs a name
-                elemName = jcrRoot;
-            } else {
-                // encode node name to make sure it's a valid xml name
-                elemName = ISO9075.encode(label);
-            }
+            // encode node name to make sure it's a valid xml name
+            elemName = ISO9075.encode(label);
+        }
 
-            // attributes (properties)
-            AttributesImpl attrs = new AttributesImpl();
-            Collections.sort(props, ItemNameComparator.INSTANCE);
-            for (Property prop: props) {
-                // attribute name (encode property name to make sure it's a valid xml name)
-                String attrName = ISO9075.encode(prop.getName());
-                Name qName = getQName(attrName);
-                boolean sort = qName.equals(NameConstants.JCR_MIXINTYPES);
-                attrs.addAttribute(qName.getNamespaceURI(), qName.getLocalName(),
-                        attrName, CDATA_TYPE, DocViewProperty.format(prop, sort, useBinaryReferences));
-            }
+        // attributes (properties)
+        AttributesImpl attrs = new AttributesImpl();
+        Collections.sort(props, ItemNameComparator.INSTANCE);
+        for (Property prop: props) {
+            // attribute name (encode property name to make sure it's a valid xml name)
+            String attrName = ISO9075.encode(prop.getName());
+            Name qName = getQName(attrName);
+            boolean sort = qName.equals(NameConstants.JCR_MIXINTYPES);
+            attrs.addAttribute(qName.getNamespaceURI(), qName.getLocalName(),
+                    attrName, CDATA_TYPE, DocViewProperty.format(prop, sort, useBinaryReferences));
+        }
 
-            // start element (node)
-            Name qName = getQName(elemName);
-            try {
-                contentHandler.startElement(qName.getNamespaceURI(),
+        // start element (node)
+        Name qName = getQName(elemName);
+        try {
+            contentHandler.startElement(qName.getNamespaceURI(),
                     qName.getLocalName(), elemName, attrs);
-            } catch (SAXException e) {
-                throw new RepositoryException(e);
-            }
+        } catch (SAXException e) {
+            throw new RepositoryException(e);
         }
     }
 
     /**
      * {@inheritDoc}
      */
+    @Override
     public void onNodeEnd(Node node, boolean included, int level) throws RepositoryException {
-        String name = node.getName();
         String label = Text.getName(node.getPath());
-        if (name.equals(jcrXMLText)) {
-            // the node represents xml character data
-            // (already processed in leavingProperties(NodeImpl, int)
-            return;
-        }
-        // element name
         String elemName;
         if (node.getDepth() == 0) {
             // root node needs a name
@@ -191,8 +291,7 @@ public class DocViewSAXFormatter extends AbstractSAXFormatter {
         // end element (node)
         Name qName = getQName(elemName);
         try {
-            contentHandler.endElement(qName.getNamespaceURI(), qName.getLocalName(),
-                    elemName);
+            contentHandler.endElement(qName.getNamespaceURI(), qName.getLocalName(), elemName);
         } catch (SAXException e) {
             throw new RepositoryException(e);
         }
@@ -201,8 +300,8 @@ public class DocViewSAXFormatter extends AbstractSAXFormatter {
     /**
      * {@inheritDoc}
      */
-    public void onProperty(Property prop, int level)
-            throws RepositoryException {
+    @Override
+    public void onProperty(Property prop, int level) throws RepositoryException {
         if (ignored.contains(prop.getName()) && prop.getDefinition().isProtected()) {
             return;
         }
@@ -213,16 +312,15 @@ public class DocViewSAXFormatter extends AbstractSAXFormatter {
     /**
      * {@inheritDoc}
      */
+    @Override
     public void onNodeIgnored(Node node, int depth) throws RepositoryException {
         // just add an empty node. used for ordering
         String label = Text.getName(node.getPath());
         String elemName = ISO9075.encode(label);
         Name qName = getQName(elemName);
         try {
-            contentHandler.startElement(qName.getNamespaceURI(),
-                qName.getLocalName(), elemName, null);
-            contentHandler.endElement(qName.getNamespaceURI(), qName.getLocalName(),
-                    elemName);
+            contentHandler.startElement(qName.getNamespaceURI(), qName.getLocalName(), elemName, null);
+            contentHandler.endElement(qName.getNamespaceURI(), qName.getLocalName(), elemName);
         } catch (SAXException e) {
             throw new RepositoryException(e);
         }

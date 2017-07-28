d6f2f6310207bf8a13ab246a65474dac866c2cfd
Tommaso Teofili <tommaso@apache.org>
2017-07-28 09:01:25 +0000

    JCRVLT-196 - this closes apache/jackrabbit-filevault#12
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/commons/filevault/trunk@1803252 13f79535-47bb-0310-9956-ffa450edef68

--- vault-core/src/main/java/org/apache/jackrabbit/vault/fs/api/package-info.java
+++ vault-core/src/main/java/org/apache/jackrabbit/vault/fs/api/package-info.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-@Version("2.4.0")
+@Version("2.5.0")
 package org.apache.jackrabbit.vault.fs.api;
 
 import org.osgi.annotation.versioning.Version;
--- vault-core/src/main/java/org/apache/jackrabbit/vault/fs/io/package-info.java
+++ vault-core/src/main/java/org/apache/jackrabbit/vault/fs/io/package-info.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-@Version("2.5.0")
+@Version("2.6.0")
 package org.apache.jackrabbit.vault.fs.io;
 
 import org.osgi.annotation.versioning.Version;

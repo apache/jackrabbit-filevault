c15446e95b1434672a7540181bc9fd0f9b0a0104
Konrad Windszus <kwin@apache.org>
2020-01-07 12:57:11 +0100

    JCRVLT-255 add failing test case for testing import modes

--- vault-core/src/test/java/org/apache/jackrabbit/vault/packaging/integration/ImportModeIT.java
+++ vault-core/src/test/java/org/apache/jackrabbit/vault/packaging/integration/ImportModeIT.java
@@ -97,4 +97,56 @@ public class ImportModeIT extends IntegrationTestBase {
         assertProperty("/tmp/foo/bar/testProperty", "new");
     }
 
+    @Test
+    public void testComplexImportModes() throws RepositoryException, IOException, PackageException {
+        // initial state
+        Node parent = admin.getRootNode().addNode("testroot");
+        setUpNode(parent, "replace");
+        setUpNode(parent, "merge");
+        setUpNode(parent, "update");
+        admin.save();
+        assertProperty("/testroot/merge/propertyold", "old");
+        assertProperty("/testroot/update/propertyold", "old");
+        
+        extractVaultPackage("/test-packages/import_modes_test_a.zip");
+        
+        // test update, creation and deletion of properties and nodes
+        
+        // Replace
+        assertProperty("/testroot/replace/propertyupdate", "new");
+        assertProperty("/testroot/replace/propertynew", "new");
+        assertPropertyMissing("/testroot/replace/propertyold");
+        assertProperty("/testroot/replace/update/propertynew", "new");
+        assertPropertyMissing("/testroot/replace/update/propertyold");
+        assertNodeExists("/testroot/replace/new");
+        assertNodeMissing("/testroot/replace/old");
+        
+        // Update (neither delete existing nodes nor properties)
+        assertProperty("/testroot/update/propertyupdate", "new");
+        assertProperty("/testroot/update/propertynew", "new");
+        assertProperty("/testroot/update/propertyold", "old");
+        assertProperty("/testroot/update/update/propertynew", "new");
+        assertProperty("/testroot/update/update/propertyold", "old");
+        assertNodeExists("/testroot/update/new");
+        assertNodeExists("/testroot/update/old");
+        
+        // Merge (don't touch existing nodes, except for adding new children)
+        assertProperty("/testroot/merge/propertyupdate", "old");
+        assertPropertyMissing("/testroot/merge/propertynew");
+        assertProperty("/testroot/merge/propertyold", "old");
+        assertPropertyMissing("/testroot/merge/update/propertynew");
+        assertProperty("/testroot/merge/update/propertyold", "old");
+        assertNodeMissing("/testroot/merge/new");
+        assertNodeExists("/testroot/merge/old");
+    }
+
+    private void setUpNode(Node parent, String name) throws RepositoryException {
+        Node node = parent.addNode(name);
+        node.setProperty("propertyold", "old");
+        node.setProperty("propertyupdate", "old");
+        node.addNode("old");
+        Node update = node.addNode("update");
+        update.setProperty("propertyold", "old");
+    }
+
 }

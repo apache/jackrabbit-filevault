6ca30f3ce7d7a0aa5c149376b43149ded1d189d1
Konrad Windszus <kwin@apache.org>
2020-01-09 17:30:08 +0000

    JCRVLT-391 fix restored backwards-compatibility in a way that let the tests pass
    git-svn-id: https://svn.apache.org/repos/asf/jackrabbit/commons/filevault/trunk@1872564 13f79535-47bb-0310-9956-ffa450edef68

--- vault-core/src/main/java/org/apache/jackrabbit/vault/util/ItemNameComparator.java
+++ vault-core/src/main/java/org/apache/jackrabbit/vault/util/ItemNameComparator.java
@@ -1,73 +1,38 @@
+
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+     * Licensed to the Apache Software Foundation (ASF) under one or more
+     * contributor license agreements.  See the NOTICE file distributed with
+     * this work for additional information regarding copyright ownership.
+     * The ASF licenses this file to You under the Apache License, Version 2.0
+     * (the "License"); you may not use this file except in compliance with
+     * the License.  You may obtain a copy of the License at
+     *
+     *      http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
 
 package org.apache.jackrabbit.vault.util;
 
 import java.util.Comparator;
 
 import javax.jcr.Item;
-import javax.jcr.RepositoryException;
-import javax.xml.namespace.QName;
 
-import org.apache.jackrabbit.spi.Name;
-import org.apache.jackrabbit.spi.commons.conversion.NameException;
-import org.apache.jackrabbit.spi.commons.conversion.NameParser;
-import org.apache.jackrabbit.spi.commons.name.NameFactoryImpl;
-import org.apache.jackrabbit.spi.commons.namespace.NamespaceResolver;
 
-/**
- * {@code ItemNameComparator}...
+/** {@code ItemNameComparator}...
+ * Use {@link ItemNameComparator2} instead
  */
+@Deprecated
+
 public class ItemNameComparator implements Comparator<Item> {
 
-    @Deprecated
     public static final ItemNameComparator INSTANCE = new ItemNameComparator();
-    
-    /**
-     * the session's namespace resolver
-     */
-    private final NamespaceResolver nsResolver;
-    
-    public ItemNameComparator(NamespaceResolver nsResolver) {
-        super();
-        this.nsResolver = nsResolver;
-    }
 
-    @Deprecated
-    public ItemNameComparator() {
-        throw new UnsupportedOperationException("This constructor is no longer supported, please use constructor with argument instead");
-    }
-
-    private QName getQName(String rawName) throws RepositoryException {
-        try {
-            Name name = NameParser.parse(rawName, nsResolver, NameFactoryImpl.getInstance());
-            return new QName(name.getNamespaceURI(), name.getLocalName(), nsResolver.getPrefix(name.getNamespaceURI()));
-        } catch (NameException e) {
-            // should never get here...
-            String msg = "internal error: failed to resolve namespace mappings";
-            throw new RepositoryException(msg, e);
-        }
-    }
-    
     public int compare(Item o1, Item o2) {
-        try {
-            return QNameComparator.INSTANCE.compare(getQName(o1.getName()), getQName(o2.getName()));
-        } catch (RepositoryException e) {
-            throw new IllegalStateException(e);
-        }
+        throw new UnsupportedOperationException("No longer supported, use ItemNameComparator2 instead");
     }
 }

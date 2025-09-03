197c21344195171c2cd523a48475cb0a9bcd501c
Jörg Hoh <joerghoh@users.noreply.github.com>
2025-09-03 11:10:07 +0200

    Update vault-core/src/main/java/org/apache/jackrabbit/vault/util/CachingSupplier.java
    Co-authored-by: Konrad Windszus <kwin@apache.org>

--- vault-core/src/main/java/org/apache/jackrabbit/vault/util/CachingSupplier.java
+++ vault-core/src/main/java/org/apache/jackrabbit/vault/util/CachingSupplier.java
@@ -23,7 +23,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * A simple utility to resolve values lazy
+A supplier wrapper which caches the supplied values.
  * @param <T>
  */
 
